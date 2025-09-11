23c76e71a472c6d69e453440aa0a93b27e7cdb00
Ashish <ashishsingh@gmail.com>
2025-09-11 15:19:35 -0700

    Checking in code for Frequency sort

---
+++ SortCharactersByFrequency/SortCharactersByFrequency/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String str = "tree";
+        System.out.println(frequencySort(str));
+    }
+
+    private static String frequencySort(String str){
+        // Count up the occurances.
+        HashMap<Character, Integer> counts = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        // Make a list of the keys, sorted by frequency.
+        ArrayList<Character> characters = new ArrayList<>(counts.keySet());
+        Collections.sort(characters, (a, b) -> counts.get(b) - counts.get(a));
+
+        // Convert the counts into a string with a sb.
+        StringBuilder sb = new StringBuilder();
+        for (char c : characters) {
+            int copies = counts.get(c);
+            for (int i = 0; i < copies; i++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
